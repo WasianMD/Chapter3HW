ea3120d1d5bcc1c89578a4df9c5d499ff9c9e414
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-10-04 18:56:15 -0400

    Update FarmAnimal.java

--- FarmAnimal.java
+++ FarmAnimal.java
@@ -1,3 +1,34 @@
 public class farmAnimal{
 
+  
+  //attributes
+  private String name;
+  private String gender;
+  private double weight; //Pounds
+  private int age;  //Years
+  
+  public Livestock(String name, String gender, double weight, int age){
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.age = age;
+  }
+    
+   public String getName(){
+        return name;
+    }
+    public void setName(String)
+    public String getGender(){
+        return gender;
+    }
+    public void setGender(String)
+    public double getWeight(){
+        return weight;
+    }
+    public void setWeight(int)
+        
+   
+    public String toString(){
+        return "Duck Quack Quack" + "\n"
+    }
 }
