e4f90db8e0c47057f62c4be56185307a98d570d9
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-10-04 19:36:07 -0400

    Update Chicken.java

--- Chicken.java
+++ Chicken.java
@@ -1,6 +1,44 @@
-public class Chicken extends FarmAnimal{
+public class Chicken extends farmAnimal{
   
   
+  private String sound
+    
+    if(gender.equals(male))
+    {
+      sound = "Cock-a-Doodle-doo";
+    }
+    else
+    {
+      sound = "Cluck Cluck";
+    }
+  
+    public Livestock(String name, String gender, double weight, int age)
+    {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.age = age;
+    }
+    
+   public String getName(){
+        return name;
+    }
+    public void setName(String)
+    public String getGender(){
+        return gender;
+    }
+    public void setGender(String)
+    public double getWeight(){
+        return weight;
+    }
+    public void setWeight(int)
+  
+  @Override
+  public String toString(){
+      return "Chicken " + sound + " [name=" + name + ", gender=" + gender + ", weight=" + weight + ", age=" + age + "]";
+    }
+  public String feedLoadingSchedule(){
+    return "8am-4pm";
   
   
   
