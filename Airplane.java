e7e3f7e5fd8b5482915cc31ae96525c792920d3d
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-10-04 21:24:04 -0400

    Create Airplane.java

---
+++ Airplane.java
@@ -0,0 +1,22 @@
+public class airplane extends flight{
+  
+  private String model;
+  private int year;
+  
+  public Plane(String model, int year)
+    {
+    this.model = name;
+    this.year = year;
+    }
+   
+  public String getModel(){
+        return model;
+  }
+  public void setModel(String)
+  public String getYear(){
+        return year;
+  }
+  public void setYear(int)
+    
+  
+}
