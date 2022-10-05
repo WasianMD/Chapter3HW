836ad4a1ff3f377b149311e969a4c98d53c91b42
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-10-04 21:42:11 -0400

    Create ThingsThatFly.java

---
+++ ThingsThatFly.java
@@ -0,0 +1,14 @@
+public class thingsThatFly extends flight{
+  
+    public static void main(String[] args){
+        Bird bird1 = new Bird();
+        bird1.setType = "Eagle";
+        
+        Bird bird2 = new Bird();
+        bird2.setType = "Hummingbird";
+      
+        Plane plane1 = new Plane();
+        plane1.setModel = "Boeing 747";
+        plane1.setYear = 2016;
+    }
+}
