1c4b04bcd8a89497939fc546916c1f909b05f353
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-10-04 21:42:08 -0400

    Update Airplane.java

--- Airplane.java
+++ Airplane.java
@@ -17,5 +17,9 @@ public class airplane extends flight{
         return year;
   }
   public void setYear(int)
+    
+    public String toString(){
+      return "Airplane [model=" + model + ", year=" + year + "]: I'm an airplane that relies on an engine to fly.";
+    }
   
 }
